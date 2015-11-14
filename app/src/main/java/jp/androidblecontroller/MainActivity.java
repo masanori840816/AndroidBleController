101aecb11f65b0821c8f4b0058e150250330c29a
masanori_msl <masanori840816@gmail.com>
2015-11-15 00:01:11 +0900

    getting permissions
    add a getting permissions function for android6.0

--- app/src/main/java/jp/androidblecontroller/MainActivity.java
+++ app/src/main/java/jp/androidblecontroller/MainActivity.java
@@ -1,11 +1,15 @@
 package jp.androidblecontroller;
 
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,6 +21,9 @@ public class MainActivity extends Activity {
     private Button mBtnOpenCentral;
     private Button mBtnOpenPeripheral;
 
+    private final static int REQUEST_PERMISSIONS = 1;
+    private final static int SDKVER_MARSHMALLOW = 23;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,11 @@ public class MainActivity extends Activity {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }
+        // Android6.0以降なら権限確認.
+        if(Build.VERSION.SDK_INT >= SDKVER_MARSHMALLOW)
+        {
+            this.requestBlePermission();
+        }
 
         mBtnOpenCentral = (Button)findViewById(R.id.btn_open_central);
         mBtnOpenCentral.setOnClickListener(mBtnOpenCentralClicked);
@@ -34,7 +46,31 @@ public class MainActivity extends Activity {
         mBtnOpenPeripheral = (Button)findViewById(R.id.btn_open_peripheral);
         mBtnOpenPeripheral.setOnClickListener(mBtnOpenPeripheralClicked);
     }
-    private final View.OnClickListener mBtnOpenCentralClicked = new View.OnClickListener() {
+    @TargetApi(SDKVER_MARSHMALLOW)
+    private void requestBlePermission(){
+        // 権限が許可されていない場合はリクエスト.
+        if(checkSelfPermission(Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions( new String[]{
+                    Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_COARSE_LOCATION
+            },REQUEST_PERMISSIONS);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // 権限リクエストの結果を取得する.
+        if (requestCode == REQUEST_PERMISSIONS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Succeed", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+            private final View.OnClickListener mBtnOpenCentralClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent ntnCentral = new Intent(MainActivity.this, CentralActivity.class);
